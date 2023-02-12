080bb00e6cbc45ae97e2d57364636bc230640954
Alexandre Schwartzmann <schwartzmann.a@gmail.com>
2023-02-12 18:08:20 +0100

    working version of frontend

--- frontend/app.py
+++ frontend/app.py
@@ -1,28 +1,48 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import requests, json
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+  if request.method == 'POST':
+    HTTPMethod = request.form.get('HTTPMethod')
+    targetURL = request.form.get('targetURL')
+    payload = json.loads(request.form.get('payload'))
+    
+    match HTTPMethod:
+      case "GET":
+        result = requests.get(targetURL)
+      case "POST":
+        result = requests.post(targetURL, data=json.dumps(payload))
+      case "PUT":
+        result = requests.put(targetURL, data=json.dumps(payload))
+      case "PATCH":
+        result = requests.patch(targetURL, data=json.dumps(payload))
+      case "DELETE":
+        result = requests.delete(targetURL, data=json.dumps(payload))
+      case _:
+        return "ERROR, HTTP Method not in (GET, POST, PUT, PATCH, DELETE)", 400
+    
+    return '''
+              <h1><u>Request:</u></h1>
+              <h2>HTTP Method: {}</h2>
+              <h2>Target URL: {}</h2>
+              <h2>Payload: {}</h2>
+              <hr \>
+              <a href="javascript:history.back()">Go Back</a><br \>
+              <h1><u>Result:</u></h1><h2> {} {}<p>Content:</h2></p>
+              {}'''.format(HTTPMethod, targetURL, payload, result.status_code, result.reason, result.text)
+   
+  print('Request for index page received')
+  return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
